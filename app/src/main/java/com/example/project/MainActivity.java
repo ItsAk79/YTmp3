3550e078f4311b76c315e0e69f301c4e419be1be
ItsAk79 <45713560+ItsAk79@users.noreply.github.com>
2020-03-31 14:56:43 +0530

    Added
    View Query added

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,15 +29,47 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
+    private EditText mQueryEt;
 
     private List<Listsong> listsongs;
 
     private List<Datum> datumList;
 
+    private SongAdapter mSongAdapter;
+
+    private List<Datum> mDatumList;
+
+    private final String TAG = "MainActivityTag";
+
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatumList = new ArrayList<>();
+        mQueryEt = findViewById(R.id.query_et);
+
+
+        mQueryEt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (mQueryEt.getRight() - mQueryEt.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // Show loader/progress
+                        getData(mQueryEt.getText().toString(),10);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
 
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
@@ -35,73 +77,35 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-
-        //recyclerView.setLayoutManager(new GridLayoutManager(this,2, GridLayoutManager.VERTICAL, false));
-
-        /*listsongs = new ArrayList<>();
-
-        listsongs.add(new Listsong("https://i.ytimg.com/vi/m_SSr-tbY_M/maxresdefault.jpg",
-                "talobrothers - Summer Air",
-                "ItaloBrothers",
-                "191"
-        ));
-
-        listsongs.add(new Listsong("https://i.ytimg.com/vi/m_SSr-tbY_M/maxresdefault.jpg",
-                "Italobrothers - Summer Air (Official Video) [Ultra Music]",
-                "ItaloBrothers",
-                "191"
-                ));
-        listsongs.add(new Listsong("https://i.ytimg.com/vi/3nLA-X54o0c/hqdefault.jpg",
-                "ItaloBrothers - Summer Air (DJ Gollum feat. DJ Cap UK Remix Video Edit)",
-                "ItaloBrothers",
-                "208"
-                ));
-        listsongs.add(new Listsong("https://i.ytimg.com/vi/3nLA-X54o0c/hqdefault.jpg",
-                "ItaloBrothers - Summer Air (DJ Gollum feat. DJ Cap UK Remix Video Edit)",
-                "ItaloBrothers",
-                "208"
-                ));
-        listsongs.add(new Listsong("https://i.ytimg.com/vi/ivCQvpaCmI4/hqdefault.jpg",
-                "Italobrothers - Summer Air [ Lyrics ]",
-                "ItaloBrothers",
-                "190"
-                ));
-
-
-
-        adapter = new Myadapter(listsongs,this);
-        recyclerView.setAdapter(adapter);*/
-
-
-      getData();
+        mSongAdapter = new SongAdapter(MainActivity.this, mDatumList);
+        recyclerView.setAdapter(mSongAdapter);
 
     }
 
-    void getData()
+    void getData(String query, Integer count)
     {
-        final Call<SongList> songlist = SongAPI.getResult().getSongList();
+        final Call<SongList> songlist = SongAPI.getResult().getSongList(query,count);
         songlist.enqueue(new Callback<SongList>() {
             @Override
             public void onResponse(Call<SongList> call, Response<SongList> response) {
-
-                //SongList list = response.body();
-                //recyclerView.setAdapter(new SongAdapter(MainActivity.this, list.getData()));
-
-                //datumdata
-                //datums.addAll((SongList)response.body().getData());
-                //songAdapter.notifyDataSetChanged();
-
                 SongList songList = response.body();
-                recyclerView.setAdapter(new SongAdapter(MainActivity.this, songList.getData()));
 
+                for(Datum mdat: songList.getData()){
+                    Log.d(TAG,mdat.getTitle());
+                }
+
+                mDatumList.removeAll(mDatumList);
+                mDatumList.addAll(songList.getData());
+                mSongAdapter.notifyDataSetChanged();
+                // Hide loader/progress
                 Toast.makeText(getApplicationContext(),"SUCCESS",Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onFailure(Call<SongList> call, Throwable t) {
-
+                //Hide loader/progress
                 Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-
+                Log.e(TAG,t.getMessage(),t);
             }
         });
     }
--- app/src/main/java/com/example/project/SongAPI.java
+++ app/src/main/java/com/example/project/SongAPI.java
@@ -1,9 +1,13 @@
 package com.example.project;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public class SongAPI {
 
@@ -15,10 +19,16 @@ public class SongAPI {
 
         if(getResult == null)
         {
+            OkHttpClient.Builder client = new OkHttpClient.Builder();
+            client.connectTimeout(40, TimeUnit.SECONDS);
+            client.readTimeout(40, TimeUnit.SECONDS);
+            client.writeTimeout(40, TimeUnit.SECONDS);
+
 
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(url)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client.build())
                     .build();
 
             getResult = retrofit.create(GetResult.class);
@@ -29,7 +39,7 @@ public class SongAPI {
     public interface GetResult{
 
         @GET("/")
-        Call<SongList> getSongList();
+        Call<SongList> getSongList(@Query("query") String query, @Query("count") Integer count);
 
     }
 
